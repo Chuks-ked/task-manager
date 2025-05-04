8f90be0c0000d9bf52e05934c8c6b068413ae3bc
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-05-04 21:23:03 +0100

    dynamic rendering

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,31 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TaskCard from './TaskCard';
+import axiosInstance from '../api/axiosinstance';
 
 const TaskList = () => {
-    const tasks = [
-        {
-        id: 1,
-        title: 'Finish report',
-        description: 'Complete the quarterly report',
-        status: 'TODO',
-        priority: 'HIGH',
-        category: { name: 'Work' },
-        },
-        {
-        id: 2,
-        title: 'Plan meeting',
-        description: 'Schedule team meeting',
-        status: 'DONE',
-        priority: 'LOW',
-        category: { name: 'Work' },
-        },
-    ];
+    const [tasks, setTasks] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchTasks = async () => {
+            try {
+                const response = await axiosInstance.get('tasks/');
+                setTasks(response.data);
+            }
+            catch (err) {
+                setError('Failed to fetch tasks. Please log in or try again later.')
+                console.error(err)
+            }
+        }
+        fetchTasks()
+    }, []);
+
+    if (error) {
+        return <div className='text-red-500'>{error}</div>
+    }
 
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {tasks.map((task) => (
-            <TaskCard key={task.id} task={task} />
-        ))}
+            {tasks.length === 0 ? (
+                <p>No tasks available.</p>
+            ) : (
+                tasks.map((task) => (
+                    <TaskCard key={task.id} task={task} />
+                ))
+            )}
         </div>
     );
 };
