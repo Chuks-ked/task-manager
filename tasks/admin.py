12d8b555d9b2aaf8af868201c2735d6bbf2a9a56
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-05-04 16:03:51 +0100

    customuser added

--- tasks/admin.py
+++ tasks/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Category, Task, CustomUser
 # Register your models here.
 
-# admin.site.register(CustomUser)
+admin.site.register(CustomUser)
 admin.site.register(Category)
 admin.site.register(Task)
