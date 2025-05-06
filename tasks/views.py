a243f5b11eee7db2d0d4aeff65fdc771e4fce76f
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-05-06 08:08:22 +0100

    filter backend set

--- tasks/views.py
+++ tasks/views.py
@@ -1,18 +1,18 @@
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Category, Task, CustomUser
 from .serializers import CategorySerializer, TaskSerializer, UserSerializer
-
 
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
 
-
 class CategoryListCreateView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return Category.objects.filter(user=self.request.user)
@@ -23,6 +23,8 @@ class CategoryListCreateView(generics.ListCreateAPIView):
 class TaskListCreateView(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
     filterset_fields = ['status', 'priority', 'category']
 
     def get_queryset(self):
@@ -30,3 +32,11 @@ class TaskListCreateView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
