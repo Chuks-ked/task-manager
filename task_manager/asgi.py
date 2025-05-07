8104fd4956a033d3344e9fb3c72fc13d069a9dd9
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-05-07 09:43:07 +0100

    ws setup

--- task_manager/asgi.py
+++ task_manager/asgi.py
@@ -8,9 +8,22 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
-from django.core.asgi import get_asgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager.settings')
 
-application = get_asgi_application()
+import django
+django.setup()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+import tasks.routing
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            tasks.routing.websocket_urlpatterns
+        )
+    ),
+})
+print("ASGI application started")
